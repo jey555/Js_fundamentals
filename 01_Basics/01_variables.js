f791790dc58edd70bf1f9d2bda0ee93a9aa15e25
Jay Pardasani <108776949+jey555@users.noreply.github.com>
2023-09-05 05:57:21 +0000

    let,const and variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountID=144553
+let accountEmail="jeypardasani555@gmail.com"
+var accountPassword="12345"
+accountCity="Amreli"
+
+//accountID=2//
+accountEmail="jayparedasanbui"
+accountPassword="212121"
+accountCity="ahmedabad"
+console.log(accountID);
+console.table([accountEmail,accountID,accountPassword,accountCity]);
