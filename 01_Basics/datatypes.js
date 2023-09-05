4a7b43629dc55f9d5cb056373ed698e3882fb6ab
Jay Pardasani <108776949+jey555@users.noreply.github.com>
2023-09-05 06:15:10 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+//alert (3+3)//we are using nodejs,not browser
+let name="hitesh"
+let age=18
+let isLoggedIn=true
+
+//number=>2 to power of 53 
+//string
+//boolean
+//symbol=>unique
+
+//object
+console.log(typeof null)
