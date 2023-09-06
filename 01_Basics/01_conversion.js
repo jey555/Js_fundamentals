1693210b64e32d2563a8320e59dd4653b2ba917e
Jay Pardasani <108776949+jey555@users.noreply.github.com>
2023-09-06 05:35:10 +0000

    conversion of datatype

---
+++ 01_Basics/01_conversion.js
@@ -0,0 +1,16 @@
+// let score='HITESH'
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score);
+// console.log(valueInNumber);
+
+//"33"=33
+//"33qbc"=NaN
+//true=1
+//null=0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
